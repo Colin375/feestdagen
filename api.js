7d129d3f4b3e2664ba130ccf162bea43a2faf0f0
Colin <Colin.Greenwood@student.gildeopleidingen.nl>
2026-06-18 14:54:27 +0200

    Comments api.js
    Added comments to api.js

--- api.js
+++ api.js
@@ -2,12 +2,15 @@ const container = document.querySelector("#feestdagen")
 const template = document.querySelector("#holiday-template");
 
 fetch('https://date.nager.at/api/v3/NextPublicHolidays/NL')
+// Check if the response is valid
 .then(response => {
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
     return response.json();
 })
+
+// Send the holiday name and date into the html
 .then(data => {
     data.forEach(holiday => {
         const clone = template.content.cloneNode(true);
@@ -21,6 +24,8 @@ fetch('https://date.nager.at/api/v3/NextPublicHolidays/NL')
         container.appendChild(clone)
             
     });
+
+    // Send the full json to the console
     console.log(data)
 })
 .catch(error => {
